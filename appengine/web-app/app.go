e1abdc6e1efc031ac36a096ecd61ed5f656fd6f3
marioharper <mario@marioharper.me>
2017-02-12 11:11:30 -0600

    start adding auth0

--- appengine/web-app/app.go
+++ appengine/web-app/app.go
@@ -1,16 +1,23 @@
 package app
 
 import (
+	"encoding/gob"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
 )
 
+var Store *sessions.CookieStore
+
 func init() {
+	Store = sessions.NewCookieStore([]byte("something-very-secret"))
+	gob.Register(map[string]interface{}{})
 	router := mux.NewRouter()
 
-	router.HandleFunc("/", IndexHandler)
 	router.PathPrefix("/public/").Handler(http.StripPrefix("/public/", http.FileServer(http.Dir("public/"))))
+	router.HandleFunc("/", IndexHandler)
+	router.HandleFunc("/callback", CallbackHandler)
 
 	http.Handle("/", router)
 }
