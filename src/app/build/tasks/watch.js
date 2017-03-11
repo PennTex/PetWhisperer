163cd65904d31964158464ca12009d44be4aad97
marioharper <mario@marioharper.me>
2017-03-11 17:07:53 -0600

    move react app to separate repo

--- src/app/build/tasks/watch.js
+++
@@ -1,6 +0,0 @@
-const gulp = require('gulp');
-const paths = require('../paths');
-
-gulp.task('watch', ['serve'], function(){
-  gulp.watch(paths.all, ['build']);
-});
--- src/app/src/main.js
+++
@@ -1,5 +0,0 @@
-import App from './components/App';
-import * as config from './config';
-
-ReactDOM.render(<App clientId={config.AUTH0_CLIENT_ID} domain={config.AUTH0_DOMAIN} />,
-  document.getElementById('app'));
